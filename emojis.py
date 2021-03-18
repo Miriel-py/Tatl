52858aca5aa230a9e6977feeec9ff9d429214d12
kklsid <wernerenz@zoneseven.ch>
2021-03-18 18:22:53 +0100

    Initial Commit

---
+++ emojis.py
@@ -0,0 +1,34 @@
+# emojis.py
+
+# Lootboxes
+lbcommon = '<:erg_commonlootbox:770547275721736223>'
+lbedgy = '<:erg_edgylootbox:770547275649777685>'
+lbepic = '<:erg_epiclootbox:770547275692507166>'
+lbgodly = '<:erg_godlylootbox:770547275704827924>'
+lbomega = '<:erg_omegalootbox:770547275726454784>'
+lbrare = '<:erg_rarelootbox:770547275709415424>'
+lbuncommon = '<:erg_uncommonlootbox:770547275528667137>'
+
+# Events
+event_arena = '<:erg_arenacookie:770324898068955186>'
+event_boss = '<:erg_ancientdragon:818542002332893204>'
+event_chop = '<:erg_woodenlog:770902085851283476>'
+event_catch = '<:erg_coin:770327166830510151>'
+event_fish = '<:erg_normiefish:770324897884536865>'
+event_miniboss = '<:erg_sword_at:770327166952538153>'
+event_summon = lbedgy
+
+
+# Misc
+bp = '<:small_yellow_diamond:818930138598080552>'
+cross = '❎'
+error = '<:redcross:796796637695311963>'
+navi_dev = '🆗'
+navi = '<:navi:796362481274060802>'
+partner_alert = '💌'
+rip = '🇫'
+fire = '🔥'
+timetraveler = '<:erg_skilltimetraveler:770327200217169962>'
+yay = '<a:yay:816581523271581696>'
+angry = '<:angry_eyes:817419266189164584>'
+sad = '<:sadcat:817418858578968596>'
